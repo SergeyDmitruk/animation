699b757f8c96b66dac7c8b5807657a841ff45398
Dmitruk_S <dmitruksl92@gmail.com>
2018-03-11 21:03:23 +0200

    Added fixed hover effect

---
+++ css3/#3-buttonHover/js/js.js
@@ -0,0 +1,12 @@
+'use string';
+
+let btn = document.querySelector('.main');
+
+btn.onmousemove = (e) => {
+
+    const x = e.pageX - e.target.offsetLeft;
+    const y = e.pageY - e.target.offsetTop;
+
+    e.target.style.setProperty('--x', `${x}px`);
+    e.target.style.setProperty('--y', `${y}px`);
+};
